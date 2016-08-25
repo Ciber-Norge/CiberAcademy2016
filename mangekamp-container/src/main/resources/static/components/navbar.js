2aeec642a9d4658d324480216285b2576ce37fbc
=Vegard Knutsen <=vegard.knutsen@ciber.com>
2016-08-25 14:44:16 +0200

    La til react routing

--- mangekamp-container/src/main/resources/static/components/navbar.js
+++ mangekamp-container/src/main/resources/static/components/navbar.js
@@ -1,14 +1,30 @@
 
 const React = require('react');
+const client = require('./../client');
+
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
 export default class Navbar extends React.Component{
+    handleLogout() {
+        client({
+            method: 'POST',
+            path: 'http://localhost:8080/logout',
+        }).then(() => {
+
+        }).done(() => {
+            console.log("Success");
+        });
+    }
+
 	render() {
 		return (
 		    <nav className="navbar">
 		        <div className="container">
 		            <ul className="navbar-list">
-		                <li className="navbar-item"><a href="#">Users</a></li>
-		                <li className="navbar-item"><a href="#">Seasons</a></li>
+		                <li className="navbar-item"><Link to={'/users'} className="navbar-brand">Mangekampen</Link></li>
+		                <li className="navbar-item"><Link to={'/users'}>Users</Link></li>
+		                <li className="navbar-item"><Link to={'/seasons'}>Seasons</Link></li>
+		                <li className="navbar-item u-pull-right "><a href="#" onClick={this.handleLogout}>Logout</a></li>
 		            </ul>
 		        </div>
 		    </nav>
---
+++ mangekamp-container/src/main/resources/static/pages/season-page.js
@@ -0,0 +1,12 @@
+
+const React = require('react');
+
+export default class SeasonPage extends React.Component{
+	render() {
+		return (
+		    <div className="container">
+		        <p>Seasons</p>
+		    </div>
+		)
+	}
+}
